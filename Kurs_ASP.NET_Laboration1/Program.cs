d3a06e004f458dac7e6bbb5069f7cae2f8248e32
mabl0200 <100345848+mabl0200@users.noreply.github.com>
2022-04-06 16:30:36 +0200

    Added 'AddEmployee' method to randomly add a person to database

--- Kurs_ASP.NET_Laboration1/Program.cs
+++ Kurs_ASP.NET_Laboration1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kurs_ASP.NET_Laboration1.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Kurs_ASP.NET_Laboration1
 {
@@ -6,7 +8,29 @@ namespace Kurs_ASP.NET_Laboration1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AddEmployee(); //Metod för att lägga till anställd i databasen
         }
+        public static void AddEmployee()
+        {
+            //Generate a random name
+            List<string> FirstNames = new List<string>() { "Anna", "Frans", "Therese", "Sune", "Sara", "Björn", "Sigrid", "Theodor", "Ellen", "Henrik", "Linnea", "Oliver" };
+            List<string> LastNames = new List<string>() { "Andersson", "Frisk", "Skog", "Eriksson", "Nordström", "Blank", "Blom", "Öberg", "Sjögren", "Eliasson", "Holm", "Berg" };
+            int randomFirstName = new Random().Next(0, 12);
+            int randomLastName = new Random().Next(0, 12);
+
+            //Add employees to db
+            using EntityBusiness context = new EntityBusiness();
+
+            Employee employee = new Employee()
+            {
+                FirstName = FirstNames[randomFirstName],
+                LastName = LastNames[randomLastName]
+            };
+
+            context.Add(employee);
+            context.SaveChanges();
+            Console.WriteLine($"Added {employee.FirstName} {employee.LastName}");
+        }
+
     }
 }
